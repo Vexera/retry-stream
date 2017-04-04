5ddae95ee90eb3b0f7806a12a4a558c2e1461b0c
luke <me@lukej.me>
2017-04-04 19:41:16 +0100

    Add files

---
+++ index.js
@@ -0,0 +1,52 @@
+const stream = require('stream');
+const https = require('https');
+
+function setupStream(inputURL, outputStream, reconnectInfo = { trys: 5, downloaded: 0, total: 0 }) {
+	if(outputStream._destroyed) return;
+	if(reconnectInfo.trys === 0) {
+		outputStream.emit('error', new Error('too many reconnects'));
+		return;
+	}
+
+	let inputStream;
+	https.get(inputURL, res => {
+		if(outputStream._destroyed) return;
+		inputStream = res;
+
+		if(reconnectInfo.downloaded === 0) {
+			reconnectInfo.total = parseInt(inputStream.headers['content-length']);
+		}
+
+		inputStream.on('data', chunk => {
+			reconnectInfo.downloaded += chunk.length;
+		});
+
+		inputStream.on('end', () => {
+			if(reconnectInfo.downloaded < reconnectInfo.total) {
+				inputStream.unpipe();
+				reconnectInfo.trys -= 1;
+
+				setupStream(inputURL, outputStream, reconnectInfo);
+			} else {
+				outputStream.end();
+			}
+		});
+
+		inputStream.pipe(outputStream);
+	});
+
+	outputStream.destroy = () => {
+		stream._destroyed = true;
+		if(inputStream) {
+			inputStream.unpipe();
+			inputStream.destroy();
+		}
+	};
+}
+
+module.exports = inputURL => {
+	let outputStream = new stream.PassThrough();
+	setupStream(inputURL, outputStream);
+
+	return outputStream;
+};
